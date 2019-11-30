1f54c4976d27be933f5a9bfe9a15cf5a9f4c63e2
lang0909 <lang0909@ajou.ac.kr>
2019-11-30 15:08:51 +0900

    start page modify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div class="logo_cont">
+      <img class="logo" src="./assets/background.jpg">
+    </div>
+    <input type="text" name="playerName" class="search_playerName" placeholder="선수이름">
+    <button type="button" class="search_button">검색</button>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
   </div>
 </template>
 
 <style>
+.logo{
+  width: 100%;
+  height: 300px;
+}
+
+.logo_cont{
+  background-color: rgb(44, 62, 80);
+}
+
+.search_playerName{
+  width: 400px;
+  height: 30px;
+  margin: 10px;
+}
+
+.search_button{
+  width: 50px;
+  height: 30px;
+  margin: 5px;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ frontend/vue.config.js
@@ -0,0 +1,9 @@
+var path = require("path")
+
+module.exports = {
+    outputDir: path.resolve(__dirname, "../backend/public/"),
+    devServer: {
+        proxy: {
+        },
+    }
+}
