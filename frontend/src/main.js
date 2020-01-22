377b60c137449929c39a80f0955ac999663a2faa
lang0909 <lang0909@ajou.ac.kr>
2020-01-22 13:23:08 +0900

    modify compare chart (passTry, passSuccess) -> (pass Success Rate)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -67,18 +67,18 @@ Vue.component('compare-bar-chart',{
   methods: {
     renderBarChart: function(){
     this.renderChart({
-      labels: ['슛', '유효슛', '도움', '골', '드리블', '패스시도', '패스성공', '차단', '태클'],
+      labels: ['슛', '유효슛', '도움', '골', '패스성공률', '차단', '태클'],
       datasets: [
         {
           label: this.position[this.data[0].spPosition]+'_'+Math.floor(this.data[0].spId/1000000)+'_'+this.first,
-          data: [this.data[0].status.shoot,this.data[0].status.effectiveShoot,this.data[0].status.assist,this.data[0].status.goal,this.data[0].status.dribble,
-          this.data[0].status.passTry,this.data[0].status.passSuccess,this.data[0].status.block,this.data[0].status.tackle],
+          data: [this.data[0].status.shoot,this.data[0].status.effectiveShoot,this.data[0].status.assist,this.data[0].status.goal,
+          this.data[0].status.passSuccess/this.data[0].status.passTry,this.data[0].status.block,this.data[0].status.tackle],
           backgroundColor: "rgba(54, 162, 235, 0.6)",
         },
         {
           label: this.position[this.data[1].spPosition]+'_'+Math.floor(this.data[1].spId/1000000)+'_'+this.second,
-          data: [this.data[1].status.shoot,this.data[1].status.effectiveShoot,this.data[1].status.assist,this.data[1].status.goal,this.data[1].status.dribble,
-          this.data[1].status.passTry,this.data[1].status.passSuccess,this.data[1].status.block,this.data[1].status.tackle],
+          data: [this.data[1].status.shoot,this.data[1].status.effectiveShoot,this.data[1].status.assist,this.data[1].status.goal,
+          this.data[1].status.passSuccess/this.data[1].status.passTry,this.data[1].status.block,this.data[1].status.tackle],
           backgroundColor: "rgba(255, 99, 132, 0.6)",
         }
       ],
