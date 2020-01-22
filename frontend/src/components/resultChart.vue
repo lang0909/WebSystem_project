5da60b6a60466217cd57aee81cffd088aabf6da3
lang0909 <lang0909@ajou.ac.kr>
2020-01-22 13:34:13 +0900

    modify result chart

--- frontend/src/components/resultChart.vue
+++ frontend/src/components/resultChart.vue
@@ -34,10 +34,9 @@ export default {
             const temp3 = Math.max.apply(Math, this.top10000.map(function(o) { return o.status.assist; }));
             const temp4 = Math.max.apply(Math, this.top10000.map(function(o) { return o.status.goal; }));
             const temp5 = Math.max.apply(Math, this.top10000.map(function(o) { return o.status.dribble; }));
-            const temp6 = Math.max.apply(Math, this.top10000.map(function(o) { return o.status.passTry; }));
-            const temp7 = Math.max.apply(Math, this.top10000.map(function(o) { return o.status.passSuccess; }));
-            const temp8 = Math.max.apply(Math, this.top10000.map(function(o) { return o.status.block; }));
-            const temp9 = Math.max.apply(Math, this.top10000.map(function(o) { return o.status.tackle; }));
+            const temp6 = Math.max.apply(Math, this.top10000.map(function(o) { return o.status.passSuccess/o.status.passTry; }));
+            const temp7 = Math.max.apply(Math, this.top10000.map(function(o) { return o.status.block; }));
+            const temp8 = Math.max.apply(Math, this.top10000.map(function(o) { return o.status.tackle; }));
             this.top10000.filter(function(temp){
                 if(temp.status.shoot===temp1){
                     temp.maxIndex.push(0);
@@ -54,17 +53,14 @@ export default {
                 if(temp.status.dribble===temp5){
                     temp.maxIndex.push(4);
                 }
-                if(temp.status.passTry===temp6){
+                if(temp.status.passSuccess/temp.status.passTry===temp6){
                     temp.maxIndex.push(5);
                 }
-                if(temp.status.passSuccess===temp7){
+                if(temp.status.block===temp8){
                     temp.maxIndex.push(6);
                 }
-                if(temp.status.block===temp8){
-                    temp.maxIndex.push(7);
-                }
                 if(temp.status.tackle===temp9){
-                    temp.maxIndex.push(8);
+                    temp.maxIndex.push(7);
                 }
             })
             this.top10000.sort(function(a,b){
